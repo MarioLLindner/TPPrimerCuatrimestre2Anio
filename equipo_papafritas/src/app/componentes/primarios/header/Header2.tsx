d802fb8a3f0518ec9c1ce12985d712c93737df21
Lindner_Mario <mario.l.lindner@gmail.com>
2024-04-09 20:58:08 -0300

    Creacion de Header(faltan modificaciones)

--- equipo_papafritas/src/app/componentes/primarios/header/Header2.tsx
+++ equipo_papafritas/src/app/componentes/primarios/header/Header2.tsx
@@ -1,18 +1,40 @@
 'use client'
 import React, { useState, useRef } from 'react'
 import './Header2.css'
-import { SelectIdiomaHeader } from './selectIdiomaHeader/SelectIdiomaHeader';
-import { SelectSearchHeader } from './buscador/selectSearch';
-/* import Logo3 from '../../../../Public/Carrousel/Logo3.png' */
+import PNGLOGO from '../../../../../Public/Logos/PNG LOGO.png'
 
 
 export const Header2 = () => {
 
+  /*   const SelectSearchHeader = ({ SelectSearch }) => {
+      const [filtro, setFiltro] = useState(""); */
+
   return (
     <>
-    <div className='container-Header'>
-          <img className='LogoHeader' src={Logo3.src} alt="CasaLindner" style={{ width: '100%', height: '75px', objectFit: 'contain' }} />
-    </div>
+      <div className='container-Header'>
+        <div>
+          <img className='LogoHeader' src={PNGLOGO.src} alt="CasaLindner" style={{ height: '75px', objectFit: 'contain' }} />
+        </div>
+        <div>
+          <div className='inputHeader'>
+            <input className='Buscador' type="text" placeholder="Que Desea Buscar?" />
+            <button className="btn-lupa" onClick={() => SelectSearch({ filtro })}>
+            </button>
+            <br />
+            <div className='LinksBuscador'>
+              <a href="">Categorias</a>
+              <a href="">Ofertas</a>
+              <a href="">Historial</a>
+              <a href="">Ayuda</a>
+              </div>
+          </div>
+        </div>
+        <div className='CuentaComprasCarro'>
+          <a href=''>Cuenta</a>
+          <a href=''>Mis compras</a>
+          <button className="btn-carrito" onClick={() => SelectSearch({ filtro })}></button>
+        </div>
+      </div>
     </>
 
   )
