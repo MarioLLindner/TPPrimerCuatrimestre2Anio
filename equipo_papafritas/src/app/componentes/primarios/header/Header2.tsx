4980c987dbe49f72b34a89122a7e8c4ff4e36ccf
Lindner_Mario <mario.l.lindner@gmail.com>
2024-04-10 23:36:03 -0300

    Arreglo Main

--- equipo_papafritas/src/app/componentes/primarios/header/Header2.tsx
+++ equipo_papafritas/src/app/componentes/primarios/header/Header2.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import './Header2.css'
 import PNGLOGO from '../../../../../Public/Logos/PNG LOGO.png'
 import { useRouter } from 'next/router'
@@ -7,39 +7,41 @@ import { usePathname } from 'next/navigation'
 import AutoCloseExample from './Dropdown/Dropdown'
 
 
-export const Header2 = (props : any) => {
+export const Header2 = (props: any) => {
 
- /*    const {url, nextPage} = props;
-    const ruta= url;
-    const router = useRouter();
-    const pathname = usePathname();
-
-      const goToCarrito = () => {
-        if (pathname !== ruta)
-          router.push(ruta)
-      } */
+  /*    const {url, nextPage} = props;
+     const ruta= url;
+     const router = useRouter();
+     const pathname = usePathname();
+ 
+       const goToCarrito = () => {
+         if (pathname !== ruta)
+           router.push(ruta)
+       } */
 
   return (
     <>
       <div className='container-Header'>
-        <div>
+        <div className='HeaderLeft'>
           <img className='LogoHeader' src={PNGLOGO.src} alt="CasaLindner" style={{ height: '75px', objectFit: 'contain' }} />
         </div>
-        <div>
-          <div className='inputHeader'>
-            <input className='Buscador' type="text" placeholder="Que Desea Buscar?" />
-            <button className="btn-lupa" onClick={() => SelectSearch({ filtro })}>
-            </button>
-            <br />
-            <div className='LinksBuscador'>
-              <a href="">Categorias</a>
-              <a href="">Ofertas</a>
-              <a href="">Historial</a>
-              <a href="">Ayuda</a>
-              </div>
+        <div className='HeaderMid'>
+          <div className='HeaderMid2'>
+            <div className='inputHeader'>
+              <input className='Buscador' type="text" placeholder="Que Desea Buscar?" />
+              <button className="btn-lupa" onClick={() => SelectSearch({ filtro })}>
+              </button>
+            </div>
           </div>
+          <div className='LinksBuscador'>
+            <a href="">Categorias</a>
+            <a href="">Ofertas</a>
+            <a href="">Historial</a>
+            <a href="">Ayuda</a>
+          </div>
+
         </div>
-        <div className='CuentaComprasCarro'>
+        <div className='HeaderRight'>
           <a href=''>Cuenta</a>
           <a href=''>Mis compras</a>
           <button className="btn-carrito" onClick={() => goToCarrito()}>{/* {nextPage} */}</button>
