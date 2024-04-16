18ea6947de3997c21964ef4ca9840f39b1fb9154
Lindner_Mario <mario.l.lindner@gmail.com>
2024-04-16 20:54:15 -0300

    Presentacion de Forms

--- equipo_papafritas/src/app/(pages)/Login/page.tsx
+++ equipo_papafritas/src/app/(pages)/Login/page.tsx
@@ -1,11 +1,28 @@
 "use client"
-
-import { Footer } from "@/app/componentes/primarios/footer/Footer";
+import { SubmitHandler, useForm } from 'react-hook-form';//FALTA INSTALAR
 import './Login.css'
 
 export default function Home() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm<UsuarioLogin>();
+  const onSubmit: SubmitHandler<UsuarioLogin> = async (datos) => {
+    const resp = await login(datos);
+    alert(resp.accessToken);
+  }
+
   return (
-    <>Login/ Register
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label className='form-label'>Usuario</label>
+          <input className='form-control' {...register("username", { required: true })} />
+        </div>
+        <div>
+          <label className='form-label'>Contraseña</label>
+          <input className='form-control' {...register("password", { required: true })} />
+        </div>
+        <input type="submit" className='btn' />
+      </form>
     </>
   );
 } 
