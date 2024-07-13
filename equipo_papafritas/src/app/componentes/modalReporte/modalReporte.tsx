50603831476d3ece04903774faa228426dba7586
kevinfrias051@gmail.com <kevinfrias051@gmail.com>
2024-07-13 12:34:27 -0300

    modal reporte

--- equipo_papafritas/src/app/componentes/modalReporte/modalReporte.tsx
+++ equipo_papafritas/src/app/componentes/modalReporte/modalReporte.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { iReporteCompras } from '@/app/model/reporte';
-/* import './modalReporte.css'; */
+import './modalReporte.css';
 
 interface ReporteModalProps {
   reporte: iReporteCompras[];
@@ -10,17 +10,26 @@ interface ReporteModalProps {
 const ReporteModal: React.FC<ReporteModalProps> = ({ reporte, onClose }) => {
   return (
     <div className="modal-background">
-      <div className="register-modal">
-        <button className="close-button" onClick={onClose}>X</button>
+      <div className="reporte-modal">
+        <div className="header-modal"> 
         <h2>Detalle Reporte</h2>
-        <div className="reporte-detalle">
+        <button className="close-button" onClick={onClose}>X</button>
+        </div>
+        <div className="list-container">
+          <div className="list-header">
+            <span>ID Compra</span>
+            <span>Id Reporte</span>
+            <span>Producto</span>
+            <span>Cantidad</span>
+            <span>Precio Producto</span>
+          </div>
           {reporte.map((compra) => (
-            <div key={compra.idCompra} className="reporte-item">
-              <p>ID Compra: {compra.idCompra}</p>
-              <p>ID Reporte: {compra.idReporte}</p>
-              <p>ID Producto: {compra.idProducto}</p>
-              <p>Cantidad: {compra.cantidad}</p>
-              <p>Precio Unitario: {compra.precioUnitario}</p>
+            <div key={compra.idCompra} className="list-item">
+              <span>{compra.idCompra}</span>
+              <span>{compra.idReporte}</span>
+              <span>{compra.idProducto}</span>
+              <span>{compra.cantidad}</span>
+              <span>{compra.precioUnitario}</span>
             </div>
           ))}
         </div>
